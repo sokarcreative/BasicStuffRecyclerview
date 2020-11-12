3c36629508197c330951e97c4a3d4b755d3c6fb1
sokarcreative <marinx.denis@gmail.com>
2020-11-12 11:54:47 +0100

    convert java file remaining to kotlin file

---
+++ library/src/androidTest/java/com/sokarcreative/basicstuffrecyclerview/ExampleInstrumentedTest.kt
@@ -0,0 +1,19 @@
+package com.sokarcreative.basicstuffrecyclerview
+
+import org.junit.Test
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+class ExampleInstrumentedTest {
+    @Test
+    @Throws(Exception::class)
+    fun useAppContext() {
+        // Context of the app under test.
+        /*Context appContext = InstrumentationRegistry.getTargetContext();
+
+        Assert.assertEquals("com.sokarcreative.library.test", appContext.getPackageName());*/
+    }
+}
